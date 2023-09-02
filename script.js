fdc3637d72c282d58052db44107abca5aefbf61a
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-09-01 20:36:39 -0400

    rebase: refatoração código

--- script.js
+++ script.js
@@ -1,68 +1,39 @@
-class ContaCorrente {
-  constructor(numero, agencia, conta, saldo) {
-    this.numero = numero;
-    this.agencia = agencia;
-    this.conta = conta;
-    this.saldo = saldo;
-  }
-
-  extrato() {
-    console.log(" ");
-    return "!!! Saldo bancário: R$" + this.saldo;
-  }
-
-  deposito(dep) {
-    if (dep < 0) {
-      console.log("Valor inválido");
-    } else {
-      this.saldo += dep;
-      console.log("Valor depositado: " + dep);
-    }
-  }
-
-  saque(saq) {
-    if (saq > this.saldo) {
-      console.log("Saldo insuficiente");
-    } else if (saq < 0) {
-      console.log("Valor inválido");
-    } else {
-      this.saldo -= saq;
-      console.log("Valor sacado: " + saq);
-    }
-  }
-}
-
-class ContaSalario extends Conta {
-  extrato() {
-    this.saldo--;
-  }
-}
+import { Pessoa, Estudante } from "./types";
 
 var cnt;
 
 function processarFormulario() {
-  var name = document.getElementById("name").value;
-  var account = document.querySelector('input[name="account"]:checked').value;
+  var nome = document.getElementById("name").value;
+  var cpf = document.getElementById("cpf").value;
+  var ende = document.getElementById("ende").value;
+  var email = document.getElementById("email").value;
+  var telefone = document.getElementById("phone").value;
+  var tipo = document.querySelector('input[name="type"]:checked').value;
+
   var textArea = document.getElementById("textArea");
-  var saldo = parseInt(Math.random() * 1000);
+  var botao = document.getElementById("botao");
 
-  switch (account) {
-    case "corrente":
-      var cnt = new Conta(parseInt(Math.random() * 10), parseInt(Math.random() * 100), name, saldo);
-
-      textArea.innerHTML = "<p>Conta corrente criada com sucesso!</p>";
-      console.log("a");
+  switch (tipo) {
+    case "estudante":
+      cnt = new Estudante(nome, cpf, ende, email, telefone);
+      textArea.innerHTML = "<p>Conta estudante criada com sucesso!</p>";
       break;
-
-    case "poupanca":
-      pessoa = new ContaPoupanca();
-      textArea.innerHTML = "<p>Conta poupança criada com sucesso!</p>";
-      console.log("b");
+    case "pessoa":
+      cnt = new Pessoa(nome, cpf, ende, email, telefone);
+      textArea.innerHTML = "<p>Conta pessoa criada com sucesso!</p>";
       break;
-
-    default:
-      textArea.innerHTML = "<p>Algo aconteceu de errado!</p>";
   }
 
+  botao.innerHTML = "<button onclick='exibirInformacoes()'>Exibir Informações</button>";
   return false;
+}
+
+function exibirInformacoes() {
+  var textArea = document.getElementById("textArea"); // Adicionando textArea aqui
+  if (cnt instanceof Estudante) {
+    textArea.innerHTML = `<p>Você possui uma conta de Estudante</p>`;
+    cnt.imprimirInformacoes();
+  } else if (cnt instanceof Pessoa) {
+    textArea.innerHTML = `<p>Você possui uma conta de Pessoa</p>`;
+  }
 }
